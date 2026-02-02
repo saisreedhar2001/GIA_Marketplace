13fbc14ffc25a50267587ff7d4c616931d6d157b
chindukuru naga sai sreedhar <61618600+saisreedhar2001@users.noreply.github.com>
2026-02-02 17:44:01 +0530

    sh2

--- api/build.sh
+++ api/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install --upgrade pip setuptools wheel
-pip install --only-binary :all: cryptography
+pip install --only-binary :all: cryptography pydantic-core
 pip install -r requirements.txt
